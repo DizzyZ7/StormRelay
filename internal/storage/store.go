d3fdc924df5fe7b7cca14d9aa3645556e3b02276
Dimash Janibekov <99655528+DizzyZ7@users.noreply.github.com>
2026-07-10 20:57:15 +0300

    feat(runbooks): advance expected schema version

--- internal/storage/store.go
+++ internal/storage/store.go
@@ -18,7 +18,7 @@ import (
 //go:embed migrations/*.sql
 var migrationFiles embed.FS
 
-const ExpectedMigrationVersion int64 = 4
+const ExpectedMigrationVersion int64 = 5
 
 type Store struct {
 	pool   *pgxpool.Pool
